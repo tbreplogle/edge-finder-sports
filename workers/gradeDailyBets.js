283ed2ee880a2c8374cd0dc6b3444ea9616d8887
Taylor Replogle <tbreplogle@gmail.com>
2025-07-14 10:58:57 -0500

    Update

--- workers/gradeDailyBets.js
+++ workers/gradeDailyBets.js
@@ -42,13 +42,12 @@ const NAME_TO_ABBR = {
   'TAMPA BAY RAYS'     : 'TBR', 'TAMPA BAY'   : 'TB',  'TB RAYS': 'TB',
   'TEXAS RANGERS'      : 'TEX', 'TEXAS'       : 'TEX',
   'TORONTO BLUE JAYS'  : 'TOR', 'TORONTO'     : 'TOR',
-  'WASHINGTON NATIONALS': 'WSH', 'WASHINGTON': 'WSH'
+  'WASHINGTON NATIONALS': 'WSH', 'WASHINGTON': 'WSH',
     /* ===== odd two-letter codes occasionally seen ===== */
     AZ :'AZ',  // Arizona
     BOS:'BOS',
     CH :'CHC', // older mobile table for Cubs
     ATH:'ATH', // Oakland in table footer
-  };
 };
 
 /* extra 2-/3-letter aliases that Covers pages sometimes use */
